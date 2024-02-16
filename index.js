4a2dd535a450b47312fae926c317dd0e11240d26
shahinexy <shahinexy@gmail.com>
2024-02-16 22:18:09 +0600

    total and card count sectin work done

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
+let totalprice = 0;
+let cardCount = 0;
 const addToCard = document.querySelectorAll('.add-btn');
-console.log(addToCard);
 
 for(const card of addToCard){
     card.addEventListener('click', function(){
+        // get card title and price
         const cardTitle = card.parentElement.childNodes[1].innerText;
         const cardPrice = card.parentNode.childNodes[3].childNodes[1].innerText;
 
-        const container = document.getElementById('selected-place-container')
+        // append title & price in cart
+        const container = document.getElementById('selected-place-container');
         const li = document.createElement('li');
         const p = document.createElement('p');
         p.innerText = cardTitle;
         const p2 = document.createElement('p');
         p2.innerText = cardPrice;
-        li.appendChild(p)
-        li.appendChild(p2)
+        li.appendChild(p);
+        li.appendChild(p2);
         container.appendChild(li);
+
+        // add to card. card count when select a card
+        cardCount += 1;
+        setinnerText('cart-count', cardCount);
+
+        // calculate total price
+        const price = parseInt(cardPrice);
+        totalprice += price;
+        setinnerText('total-cost', totalprice);
 
     })
 }
