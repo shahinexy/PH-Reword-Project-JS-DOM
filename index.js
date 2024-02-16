e60cfb3dead2e0fdc6b2c85834c88d0f309b0d5e
shahinexy <shahinexy@gmail.com>
2024-02-16 23:21:11 +0600

    budget calculation completed

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ let totalprice = 0;
 let cardCount = 0;
 const addToCard = document.querySelectorAll('.add-btn');
 
-for(const card of addToCard){
-    card.addEventListener('click', function(){
+for (const card of addToCard) {
+    card.addEventListener('click', function () {
+
         // get card title and price
         const cardTitle = card.parentElement.childNodes[1].innerText;
         const cardPrice = card.parentNode.childNodes[3].childNodes[1].innerText;
+
+        // Check budget grater than coust otherwise stop the function (validation)
+        const budget = document.getElementById('budget').innerText;
+        if (budget < parseInt(cardPrice)) {
+            alert('You need more money')
+            return;
+        }
 
         // append title & price in cart
         const container = document.getElementById('selected-place-container');
@@ -28,5 +36,24 @@ for(const card of addToCard){
         totalprice += price;
         setinnerText('total-cost', totalprice);
 
+        // check budget
+        const updatedBudget = parseInt(budget) - price;
+        setinnerText('budget', updatedBudget)
+
     })
+}
+
+function grandTotal(vehicle) {
+    if (vehicle == 'bus') {
+        const busPrice = totalprice + 100;
+        setinnerText('grand-total', busPrice)
+    }
+    else if (vehicle == 'train') {
+        const busPrice = totalprice - 200;
+        setinnerText('grand-total', busPrice)
+    }
+    else if (vehicle == 'flight') {
+        const busPrice = totalprice + 500;
+        setinnerText('grand-total', busPrice)
+    }
 }
